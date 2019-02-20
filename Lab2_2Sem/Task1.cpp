8ba8452074aba1b65919f168303a96423461caa9
Gleb Lagutko <gleblagutko@gmail.com>
2019-02-20 16:35:04 +0300

    1

--- Lab2_2Sem/Task1.cpp
+++ Lab2_2Sem/Task1.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <set>
-#include <map>
+#include <vector>
 #include <algorithm>
 #include <fstream>
 #include <algorithm>
 #include <sstream>
 #include <codecvt>
-#include <vector>
+#include <list>
+
 using namespace std;
 
-
-
-void PrintSet(set<wstring>& set)
+void PrintSet(vector<wstring>& set)
 {
-	for (auto iterator = set.begin(); iterator != set.end(); ++iterator)
-		wcout << *iterator << endl;
+	for (auto it = set.begin(); it != set.end(); ++it)
+		wcout << *it << endl ;
 }
 
-void Print(set<wstring>& set1, set<wstring>& set2, set<wstring>& set3, set<wstring>& set4)
+void Print(vector<wstring>& set1, vector<wstring>& set2, vector<wstring>& set3, vector<wstring>& set4)
 {
 	wcout << L" 1 :";
 	PrintSet(set1);
@@ -29,73 +28,74 @@ void Print(set<wstring>& set1, set<wstring>& set2, set<wstring>& set3, set<wstri
 	PrintSet(set4);
 }
 
-/*set<wstring> Difference(set<wstring>& set1, set<wstring>& set2, set<wstring>& set3)
+vector<wstring> Difference(vector<wstring>& set1, vector<wstring>& set2, vector<wstring>& set3)
 {
-	set<wstring> SetDifference;
-	set_difference(set1.cbegin(), set1.cend(), set2.cbegin(), set2.cend(), SetDifference.begin());
-	set_difference(SetDifference.cbegin(), SetDifference.cend(), set3.cbegin(), set3.cend(), SetDifference.begin());
-	
+	vector<wstring> SetDifference;
+	SetDifference.end() = set_difference(set1.begin(), set1.end(), set2.begin(), set2.end(), SetDifference.begin());	
+	SetDifference.end() = set_difference(SetDifference.begin(), SetDifference.end(), set3.begin(), set3.end(), SetDifference.begin());
 
 	return SetDifference;
 }
 
-set<wstring> SymmetricDifference(set<wstring>& set1, set<wstring>& set2, set<wstring>& set3)
+vector<wstring> SymmetricDifference(vector<wstring>& set1, vector<wstring>& set2, vector<wstring>& set3)
 {
-	set<wstring> SetSymmetricDifference;
-	set_symmetric_difference(set1.cbegin(), set1.cend(), set2.cbegin(), set2.cend(), SetSymmetricDifference.begin());
-	set_symmetric_difference(set3.cbegin(), set3.cend(), SetSymmetricDifference.cbegin(), SetSymmetricDifference.cend(), SetSymmetricDifference.begin());
+	vector<wstring> SetSymmetricDifference;
+	SetSymmetricDifference.end() = set_symmetric_difference(set1.begin(), set1.end(), set2.begin(), set2.end(), SetSymmetricDifference.begin());
+	SetSymmetricDifference.end() = set_symmetric_difference(set3.begin(), set3.end(), SetSymmetricDifference.begin(), SetSymmetricDifference.end(), SetSymmetricDifference.begin());
+
 	return SetSymmetricDifference;
 }
 
-set<wstring> Union(set<wstring>& set1, set<wstring>& set2, set<wstring>& set3)
+vector<wstring> Union(vector<wstring>& set1, vector<wstring>& set2, vector<wstring>& set3)
 {
-	set<wstring> SetUnion;
-	set_union(set1.cbegin(), set1.cend(), set2.cbegin(), set2.cend(), SetUnion.begin());
-	set_union(set3.cbegin(), set3.cend(), SetUnion.cbegin(), SetUnion.cend(), SetUnion.begin());
+	vector<wstring> SetUnion(set1.size() + set2.size() + set3.size());
+	SetUnion.end() = set_union(set1.begin(), set1.end(), set2.begin(), set2.end(), SetUnion.begin());
+    SetUnion.end() = set_union(set3.begin(), set3.end(), SetUnion.begin(), SetUnion.end(), SetUnion.begin());
 	return SetUnion;
 }
 
-set<wstring> Intersection(set<wstring>& set1, set<wstring>& set2, set<wstring>& set3)
+vector<wstring> Intersection(vector<wstring>& set1, vector<wstring>& set2, vector<wstring>& set3)
 {
-	set<wstring> SetIntersection;
-	set_intersection(set1.cbegin(), set1.cend(), set2.cbegin(), set2.cend(), SetIntersection.begin());
-	set_intersection(set3.cbegin(), set3.cend(), SetIntersection.cbegin(), SetIntersection.cend(), SetIntersection.begin());
+	vector<wstring> SetIntersection;
+	SetIntersection.end() = set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(), SetIntersection.begin());	
+	SetIntersection.end() = set_intersection(set3.begin(), set3.end(), SetIntersection.begin(), SetIntersection.end(), SetIntersection.begin());
 	return SetIntersection;
-}*/
+}
 
-set<wstring> FillSet(wstring name)
+vector<wstring> FillSet(wstring name)
 {
 	wifstream wifile;
-	set<wstring> wset;
+	vector<wstring> wset;
+	wstring string;
 	wifile.open(name);
 	wifile.imbue(locale(locale::empty(), new codecvt_utf8<wchar_t>));
-	while (!wifile.eof())
+	while (wifile.good())
 	{
-		    wstring string;
 			getline(wifile, string);
-			wset.insert(string);
+			if(!string.empty())
+			wset.push_back(string);
 	}
 	return wset;
 }
 
+void SortVector(vector<wstring> vector)
+{
+	sort(vector.begin(),vector.end());
+}
+
 int main()
 {
-	set<wstring> Lake1 = FillSet(L"C:\\Users\\Dell\\source\\repos\\Lab1_2Sem\\lake1.txt");
-	set<wstring> Lake2 = FillSet(L"C:\\Users\\Dell\\source\\repos\\Lab1_2Sem\\lake2.txt");
-	set<wstring> Lake3 = FillSet(L"C:\\Users\\Dell\\source\\repos\\Lab1_2Sem\\lake3.txt");
-	set<int> SetIntersection;
-	std::set<int>::iterator it;
-	//set_intersection(Lake1.cbegin(), Lake1.cend(), Lake2.cbegin(), Lake2.cend(), SetIntersection.begin());
-
-	int first[] = { 5,10,15,20,25 };
-	int second[] = { 50,40,30,20,10 };
-
-	it = std::set_symmetric_difference(first, first + 5, second, second + 5, SetIntersection.begin());
-	/*set<wstring> SetUnion = Union(Lake1,Lake2,Lake3);
-	set<wstring> SetIntersection = Intersection(Lake1, Lake2, Lake3);
-	set<wstring> SetIndividual = SymmetricDifference(Lake1, Lake2, Lake3);
-	set<wstring> SetFirst = Difference(Lake1,Lake2,Lake3) ; 
-	Print(SetUnion, SetIntersection, SetIndividual, SetFirst);*/
+	vector<wstring> Lake1 = FillSet(L"C:\\Users\\Dell\\source\\repos\\Lab1_2Sem\\lake1.txt");
+	vector<wstring> Lake2 = FillSet(L"C:\\Users\\Dell\\source\\repos\\Lab1_2Sem\\lake2.txt");
+	vector<wstring> Lake3 = FillSet(L"C:\\Users\\Dell\\source\\repos\\Lab1_2Sem\\lake3.txt");
+	SortVector(Lake1);
+	SortVector(Lake2);
+	SortVector(Lake3);
+	vector<wstring> SetUnion = Union(Lake1,Lake2,Lake3);
+	vector<wstring> SetIntersection = Intersection(Lake1, Lake2, Lake3);
+	vector<wstring> SetIndividual = SymmetricDifference(Lake1, Lake2, Lake3);
+	vector<wstring> SetFirst = Difference(Lake1,Lake2,Lake3) ; 
+	Print(SetUnion, SetIntersection, SetIndividual, SetFirst);
 
 	system("pause");
 	return 0;
